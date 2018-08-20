e3d1a38227a1365e5a2772c726a3553ad45180a3
NickLojas <nick.lojas@mail.utoronto.ca>
2018-08-19 22:32:06 -0400

    Added MockStack Project

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MockStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            //passing 3 different objects
+            stack.Push(null);
+            stack.Push(2);
+            stack.Push("third object");
+
+            Console.WriteLine(stack.Pop()); // should firstly pop the third item
+            Console.WriteLine(stack.Pop());
+            
+            Console.WriteLine(stack.Pop()); // nothing left in the stack to pop. should display a exception message.
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MockStack
+{
+    class Stack
+    {
+        public List<object> StackList { get; private set; } = new List<object>();
+
+        public void Push(object obj)
+        {
+            try
+            {
+                if (obj.Equals(null))
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+                    StackList.Add(obj);
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Cannot push a null object to the Stack.");
+            }
+        }
+
+        public object Pop()
+        {
+            try
+            {
+                if (StackList.Count == 0)
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+                    var element = StackList[StackList.Count - 1];
+                    StackList.RemoveAt(StackList.Count - 1);
+                    return element;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Must have an object in the stack to pop.");
+                return null;
+            }
+        }
+
+        public void Clear()
+        {
+            StackList.Clear();
+        }
+
+    }
+}
